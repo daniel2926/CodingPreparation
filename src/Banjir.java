d38a0b212f62ce9c83497e4f818c1f8448d5c90e
Daniel <danielsimamorasimamora@gmail.com>
2025-06-17 09:54:52 +0700

    banjir

---
+++ src/Banjir.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Banjir {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int T = input.nextInt();
+
+
+
+        for (int i = 0; i < T; i++) {
+            int N = input.nextInt();
+            int M = input.nextInt();
+            for (int j = 0; j < N; j++) {
+                String pagar = input.next();
+            }
+        }
+    }
+}
--- src/PetakMenarik.java
+++ src/PetakMenarik.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class PetakMenarik {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        int row = input.nextInt();
+        int column = input.nextInt();;
+        int K = input.nextInt();
 
 
-        int[][] arr = new int[2][2];
-        arr[0][0] = 3;
-        arr[0][1] = 1;
-        arr[1][0] = 1;
-        arr[1][1] = 2;
-        int resultMultiply = 1;
+        int [][] arr = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = input.nextInt();
+            }
+        }
+        int resultRow = 0;
+        int resultColumn = 0;
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++){
+                int resultMultiply = 1;
                 int kanan,kiri,atas,bawah;
                 if (i - 1 >= 0){
                     atas =arr[i - 1][j];
@@ -30,8 +39,19 @@ public class PetakMenarik {
                     kanan = arr[i][j + 1];
                     resultMultiply *= kanan;
                 }
-                System.out.println(resultMultiply);
+
+                if(K == resultMultiply){
+                    if (resultRow == 0 || j + 1 < resultColumn || (j + 1 == resultColumn && i + 1 < resultRow)){
+                        resultRow = i + 1;
+                        resultColumn = j + 1;
+                    }
+                }
             }
         }
+        if(resultRow == 0){
+            System.out.println("0 0");
+        }
+        else
+            System.out.println(resultRow + " " + resultColumn);
     }
 }
